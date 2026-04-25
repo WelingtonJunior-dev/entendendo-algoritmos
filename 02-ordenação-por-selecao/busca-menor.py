423056032c58019a595ce052daf323e0fd4764d1
Welington Morais Ferreira Junior <welmfj@gmail.com>
2026-04-24 23:53:30 -0300

    Algorimo de ordenação simples

---
+++ 02-ordenação-por-selecao/busca-menor.py
@@ -0,0 +1,10 @@
+# Esse código faz a ordenação de um array do menor para o maior elemento
+
+def buscaMenor(arr):
+    menor = arr[0]
+    menor_indice = 0
+    for i in range(1, len(arr)):
+        if arr[i] < menor:
+            menor = arr[i]
+            menor_indice = i
+    return menor_indice
